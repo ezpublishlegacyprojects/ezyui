fb93a908f6dc81d81132272bc7efded53923ee20
andre1 <info@example.com>
2009-06-08 12:09:17 +0000

    removed php version of json_encode, so requriment is now PHP 5.2 or PHP 5.1 with json extension
    git-svn-id: http://svn.projects.ez.no/ezyui@4 710d20e5-485d-47fa-ac04-bc0a43c84fa1

--- trunk/ezyui/classes/ezyuiajaxcontent.php
+++ trunk/ezyui/classes/ezyuiajaxcontent.php
@@ -125,7 +125,7 @@ class eZYuiAjaxContent
         if ( $type === 'xml' )
             return self::xmlEncode( $ret );
         else if ( $type === 'json' )
-            return self::jsonEncode( $ret );
+            return json_encode( $ret );
         else
             return self::textEncode( $ret );
     }
@@ -140,6 +140,7 @@ class eZYuiAjaxContent
     {
         if ( is_array( $mix ) )
             return implode(',', array_map( array('eZYuiAjaxContent', 'textEncode'), array_filter( $mix ) ) );
+
         return $mix;
     }
 
@@ -170,7 +171,7 @@ class eZYuiAjaxContent
         if ( $type === 'xml' )
             return self::xmlEncode( $ret );
         else if ( $type === 'json' )
-            return self::jsonEncode( $ret );
+            return json_encode( $ret );
         else
             return $ret;
     }
@@ -396,203 +397,6 @@ class eZYuiAjaxContent
                 $xml->writeElement( $key, $value );
             }
         }
-    }
-
-    /**
-     * Wrapper function for encoding to json with native or php version
-     * depending on what the system supports
-     * 
-     * @param mixed $obj
-     * @return string
-    */
-    public static function jsonEncode( $obj )
-    {
-        if ( self::$nativeJsonEncode === null )
-            self::$nativeJsonEncode = function_exists( 'json_encode' );
-
-        if ( self::$nativeJsonEncode === true )
-            return json_encode( $obj );
-
-        $inst = self::getInstance();
-        return $inst->phpJsonEncode( $obj );    
-    }
-    
-    
-    /**
-     * Returns the JSON representation of a value using php code
-     * 
-     * @param mixed $var
-     * @author Michal Migurski <mike-json@teczno.com>
-     * @author Matt Knapp <mdknapp[at]gmail[dot]com>
-     * @author Brett Stimmerman <brettstimmerman[at]gmail[dot]com>
-     * @copyright 2005 Michal Migurski
-     * @license http://www.freebsd.org/copyright/freebsd-license.html
-     * @link http://pear.php.net/pepr/pepr-proposal-show.php?id=198
-     * @access protected
-     * @return string
-    */
-    protected function phpJsonEncode( $var )
-    {       
-        switch (gettype($var)) {
-            case 'boolean':
-                return $var ? 'true' : 'false';
-
-            case 'NULL':
-                return 'null';
-
-            case 'integer':
-                return sprintf('%d', $var);
-
-            case 'double':
-            case 'float':
-                return sprintf('%f', $var);
-
-            case 'string':
-                // STRINGS ARE EXPECTED TO BE IN ASCII OR UTF-8 FORMAT
-                $ascii = '';
-                $strlen_var = strlen($var);
-
-               /*
-                * Iterate over every character in the string,
-                * escaping with a slash or encoding to UTF-8 where necessary
-                */
-                for ($c = 0; $c < $strlen_var; ++$c) {
-
-                    $ord_var_c = ord($var{$c});
-
-                    switch ($ord_var_c) {
-                        case 0x08:  $ascii .= '\b';  break;
-                        case 0x09:  $ascii .= '\t';  break;
-                        case 0x0A:  $ascii .= '\n';  break;
-                        case 0x0C:  $ascii .= '\f';  break;
-                        case 0x0D:  $ascii .= '\r';  break;
-
-                        case 0x22:
-                        case 0x2F:
-                        case 0x5C:
-                            // double quote, slash, slosh
-                            $ascii .= '\\'.$var{$c};
-                            break;
-
-                        case (($ord_var_c >= 0x20) && ($ord_var_c <= 0x7F)):
-                            // characters U-00000000 - U-0000007F (same as ASCII)
-                            $ascii .= $var{$c};
-                            break;
-
-                        case (($ord_var_c & 0xE0) == 0xC0):
-                            // characters U-00000080 - U-000007FF, mask 110XXXXX
-                            // see http://www.cl.cam.ac.uk/~mgk25/unicode.html#utf-8
-                            $char = pack('C*', $ord_var_c, ord($var{$c+1}));
-                            $c+=1;
-                            $utf16 = mb_convert_encoding($char, 'UTF-16', 'UTF-8');
-                            $ascii .= sprintf('\u%04s', bin2hex($utf16));
-                            break;
-
-                        case (($ord_var_c & 0xF0) == 0xE0):
-                            // characters U-00000800 - U-0000FFFF, mask 1110XXXX
-                            // see http://www.cl.cam.ac.uk/~mgk25/unicode.html#utf-8
-                            $char = pack('C*', $ord_var_c,
-                                         ord($var{$c+1}),
-                                         ord($var{$c+2}));
-                            $c+=2;
-                            $utf16 = mb_convert_encoding($char, 'UTF-16', 'UTF-8');
-                            $ascii .= sprintf('\u%04s', bin2hex($utf16));
-                            break;
-
-                        case (($ord_var_c & 0xF8) == 0xF0):
-                            // characters U-00010000 - U-001FFFFF, mask 11110XXX
-                            // see http://www.cl.cam.ac.uk/~mgk25/unicode.html#utf-8
-                            $char = pack('C*', $ord_var_c,
-                                         ord($var{$c+1}),
-                                         ord($var{$c+2}),
-                                         ord($var{$c+3}));
-                            $c+=3;
-                            $utf16 = mb_convert_encoding($char, 'UTF-16', 'UTF-8');
-                            $ascii .= sprintf('\u%04s', bin2hex($utf16));
-                            break;
-
-                        case (($ord_var_c & 0xFC) == 0xF8):
-                            // characters U-00200000 - U-03FFFFFF, mask 111110XX
-                            // see http://www.cl.cam.ac.uk/~mgk25/unicode.html#utf-8
-                            $char = pack('C*', $ord_var_c,
-                                         ord($var{$c+1}),
-                                         ord($var{$c+2}),
-                                         ord($var{$c+3}),
-                                         ord($var{$c+4}));
-                            $c+=4;
-                            $utf16 = mb_convert_encoding($char, 'UTF-16', 'UTF-8');
-                            $ascii .= sprintf('\u%04s', bin2hex($utf16));
-                            break;
-
-                        case (($ord_var_c & 0xFE) == 0xFC):
-                            // characters U-04000000 - U-7FFFFFFF, mask 1111110X
-                            // see http://www.cl.cam.ac.uk/~mgk25/unicode.html#utf-8
-                            $char = pack('C*', $ord_var_c,
-                                         ord($var{$c+1}),
-                                         ord($var{$c+2}),
-                                         ord($var{$c+3}),
-                                         ord($var{$c+4}),
-                                         ord($var{$c+5}));
-                            $c+=5;
-                            $utf16 = mb_convert_encoding($char, 'UTF-16', 'UTF-8');
-                            $ascii .= sprintf('\u%04s', bin2hex($utf16));
-                            break;
-                    }
-                }
-                
-                return '"'.$ascii.'"';
-
-            case 'array':
-               /*
-                * As per JSON spec if any array key is not an integer
-                * we must treat the the whole array as an object. We
-                * also try to catch a sparsely populated associative
-                * array with numeric keys here because some JS engines
-                * will create an array with empty indexes up to
-                * max_index which can cause memory issues and because
-                * the keys, which may be relevant, will be remapped
-                * otherwise.
-                * 
-                * As per the ECMA and JSON specification an object may
-                * have any string as a property. Unfortunately due to
-                * a hole in the ECMA specification if the key is a
-                * ECMA reserved word or starts with a digit the
-                * parameter is only accessible using ECMAScript's
-                * bracket notation.
-                */
-
-                // treat as a JSON object  
-                if (is_array($var) && count($var) && (array_keys($var) !== range(0, sizeof($var) - 1))) {
-                    return sprintf('{%s}', join(',', array_map(array($this, 'phpJsonEncodeNameValue'),
-                                                               array_keys($var),
-                                                               array_values($var))));
-                }
-
-                // treat it like a regular array
-                return sprintf('[%s]', join(',', array_map(array($this, 'phpJsonEncode'), $var)));
-
-            case 'object':
-                $vars = get_object_vars($var);
-                return sprintf('{%s}', join(',', array_map(array($this, 'phpJsonEncodeNameValue'),
-                                                           array_keys($vars),
-                                                           array_values($vars))));                    
-
-            default:
-                return '';
-        }
-    }
-
-   /**
-    * array-walking function for use in generating JSON-formatted name-value pairs
-    * 
-    * @param string $name
-    * @param mixed $value
-    * @access protected
-    * @return string
-    */
-    protected function phpJsonEncodeNameValue( $name, $value )
-    {
-        return (sprintf("%s:%s", $this->phpJsonEncode(strval($name)), $this->phpJsonEncode($value)));
     }
 }
 
